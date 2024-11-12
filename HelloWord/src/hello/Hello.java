20bf9e43041d3b95aac196a04a9a7a28912ad1e8
pedromst2000 <40210465@esmad.ipp.pt>
2024-11-12 15:41:25 +0000

    chore: Added HelloWorld init project

---
+++ HelloWord/src/hello/Hello.java
@@ -0,0 +1,14 @@
+package hello;
+
+/**
+ * The Hello class implements an application that simply prints "Hello, World!"
+ * to standard output.
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello\n");
+        System.out.println("World !");
+
+    }
+}
